fc981ae78bcc140a6b62b56e732d08bc3ce2a333
fatCat <562204948@qq.com>
2018-05-21 16:33:15 +0800

    Rejected commit: Default

---
+++ src/main/java/com/springmvc/utilis/ResultUtil.java
@@ -0,0 +1,24 @@
+package com.springmvc.utilis;
+
+public class ResultUtil {
+    //当正确时返回的值
+    public static Result success(Object data){
+        Result result = new Result();
+        result.setRetCode(0);
+        result.setMessage("OK");
+        result.setData(data);
+        return result;
+    }
+
+    public static Result success(){
+        return success(null);
+    }
+
+    //当错误时返回的值
+    public static Result error(int code,String msg){
+        Result result = new Result();
+        result.setRetCode(code);
+        result.setMessage(msg);
+        return result;
+    }
+}
